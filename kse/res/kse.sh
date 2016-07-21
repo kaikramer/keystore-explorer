bf353d2ea0f3fa0eea37ffd4ba41c2b9441daa57
Josef Ludvicek <jludvice@redhat.com>
2016-07-21 12:33:11 +0200

    [closes #39] Improve kse.sh DIRNAME

--- kse/res/kse.sh
+++ kse/res/kse.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# Oracle/OpenJDK JRE version 1.6+ has to be present on your path 
-# OR in a directory called "jre" in the same directory as this script 
+# Oracle/OpenJDK JRE version 1.6+ has to be present on your path
+# OR in a directory called "jre" in the same directory as this script
 
-SCRIPT_DIR="`dirname \"$0\"`"
+# detect parent folder of script, regardless aliases and symlinks
+SOURCE="${BASH_SOURCE[0]}"
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 JAR_FILE="${SCRIPT_DIR}/kse.jar"
 
 if [ -d "${SCRIPT_DIR}/jre" ]; then
@@ -11,4 +21,3 @@ if [ -d "${SCRIPT_DIR}/jre" ]; then
 else
     java -jar "${JAR_FILE}" "$@"
 fi
-
