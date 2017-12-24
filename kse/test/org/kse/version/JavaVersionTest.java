f90f8a9cb540441c1c733154194459e4ae7393df
Kai Kramer <kaikramer@users.noreply.github.com>
2017-12-24 19:06:22 +0100

    Migrated JavaVersionTest to JUnit5

--- kse/test/org/kse/version/JavaVersionTest.java
+++ kse/test/org/kse/version/JavaVersionTest.java
@@ -1,57 +1,99 @@
 package org.kse.version;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.kse.version.JavaVersion;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class JavaVersionTest {
 
-	@Test
-	public void test() {
-		assertEquals(JavaVersion.JRE_VERSION_160, new JavaVersion("1.6.0"));
-		assertTrue(JavaVersion.JRE_VERSION_160.compareTo(new JavaVersion("1.6.0_45")) == 0);
-
-		assertTrue(JavaVersion.JRE_VERSION_170.compareTo(new JavaVersion("1.6.0_45")) > 0);
-		assertEquals(JavaVersion.JRE_VERSION_170, new JavaVersion("1.7.0"));
-		assertTrue(JavaVersion.JRE_VERSION_170.compareTo(new JavaVersion("1.7.0_101")) == 0);
-
-		assertTrue(JavaVersion.JRE_VERSION_180.compareTo(new JavaVersion("1.7.0_101")) > 0);
-		assertEquals(JavaVersion.JRE_VERSION_180, new JavaVersion("1.8.0"));
-		assertTrue(JavaVersion.JRE_VERSION_180.compareTo(new JavaVersion("1.8.0_20")) == 0);
-
-		assertTrue(JavaVersion.JRE_VERSION_160.isBelow(JavaVersion.JRE_VERSION_170));
-		assertTrue(JavaVersion.JRE_VERSION_170.isBelow(JavaVersion.JRE_VERSION_180));
-		assertTrue(JavaVersion.JRE_VERSION_180.isBelow(JavaVersion.JRE_VERSION_9));
-		assertFalse(JavaVersion.JRE_VERSION_9.isBelow(JavaVersion.JRE_VERSION_180));
-		assertFalse(JavaVersion.JRE_VERSION_170.isBelow(JavaVersion.JRE_VERSION_160));
-		assertFalse(JavaVersion.JRE_VERSION_170.isBelow(JavaVersion.JRE_VERSION_170));
-		assertFalse(JavaVersion.JRE_VERSION_180.isBelow(JavaVersion.JRE_VERSION_160));
-		assertFalse(JavaVersion.JRE_VERSION_180.isBelow(JavaVersion.JRE_VERSION_170));
-		assertFalse(JavaVersion.JRE_VERSION_180.isBelow(JavaVersion.JRE_VERSION_180));
-
-		assertTrue(JavaVersion.JRE_VERSION_9.isAtLeast(JavaVersion.JRE_VERSION_160));
-		assertTrue(JavaVersion.JRE_VERSION_9.isAtLeast(JavaVersion.JRE_VERSION_170));
-		assertTrue(JavaVersion.JRE_VERSION_9.isAtLeast(JavaVersion.JRE_VERSION_170));
-		assertTrue(JavaVersion.JRE_VERSION_9.isAtLeast(JavaVersion.JRE_VERSION_9));
-		assertTrue(JavaVersion.JRE_VERSION_180.isAtLeast(JavaVersion.JRE_VERSION_160));
-		assertTrue(JavaVersion.JRE_VERSION_180.isAtLeast(JavaVersion.JRE_VERSION_170));
-		assertTrue(JavaVersion.JRE_VERSION_180.isAtLeast(JavaVersion.JRE_VERSION_180));
-		assertFalse(JavaVersion.JRE_VERSION_180.isAtLeast(JavaVersion.JRE_VERSION_9));
-		assertTrue(JavaVersion.JRE_VERSION_170.isAtLeast(JavaVersion.JRE_VERSION_160));
-		assertTrue(JavaVersion.JRE_VERSION_170.isAtLeast(JavaVersion.JRE_VERSION_170));
-		assertFalse(JavaVersion.JRE_VERSION_170.isAtLeast(JavaVersion.JRE_VERSION_180));
-		assertFalse(JavaVersion.JRE_VERSION_170.isAtLeast(JavaVersion.JRE_VERSION_9));
-
-		assertEquals(1, new JavaVersion("1.8.0_20-b62").getMajor());
-		assertEquals(8, new JavaVersion("1.8.0_20-b62").getMinor());
-		assertEquals(0, new JavaVersion("1.8.0_20-b62").getSecurity());
-
-		assertEquals(9, new JavaVersion("9").getMajor());
-		assertEquals(0, new JavaVersion("9").getMinor());
-		assertEquals(9, new JavaVersion("9-ea").getMajor());
+	@ParameterizedTest
+	@CsvSource({
+		"JRE_VERSION_160, 1.6.0",
+		"JRE_VERSION_170, 1.7.0",
+		"JRE_VERSION_180, 1.8.0",
+	})
+	public void equals(@ConvertWith(JavaVersionConstantArgumentConverter.class) JavaVersion verConst, String verStr) {
+		assertEquals(verConst, new JavaVersion(verStr));
 	}
 
+	@ParameterizedTest
+	@CsvSource({
+		"JRE_VERSION_160, 1.6.0_45, 0",
+		"JRE_VERSION_170, 1.6.0_45, 1",
+		"JRE_VERSION_170, 1.7.0_101, 0",
+		"JRE_VERSION_180, 1.7.0_101, 1",
+		"JRE_VERSION_180, 1.8.0_20, 0",
+		"JRE_VERSION_170, 1.8.0_20, -1",
+		"JRE_VERSION_160, 1.8.0_20, -1",
+	})
+	public void compareTo(@ConvertWith(JavaVersionConstantArgumentConverter.class) JavaVersion verConst, String verStr,
+			int result) {
+		assertEquals(Integer.signum(verConst.compareTo(new JavaVersion(verStr))), result);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"1.8.0_20-b62, 	1, 8, 0",
+		"9, 			9, 0, 0",
+		"9-ea, 			9, 0, 0",
+	})
+	public void getMajorMinorSecurity(String ver, int major, int minor, int security) {
+		assertEquals(major, new JavaVersion(ver).getMajor());
+		assertEquals(minor, new JavaVersion(ver).getMinor());
+		assertEquals(security, new JavaVersion(ver).getSecurity());
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"JRE_VERSION_9,   JRE_VERSION_160, true",
+		"JRE_VERSION_9,   JRE_VERSION_170, true",
+		"JRE_VERSION_9,   JRE_VERSION_180, true",
+		"JRE_VERSION_9,   JRE_VERSION_9,   true",
+		"JRE_VERSION_180, JRE_VERSION_160, true",
+		"JRE_VERSION_180, JRE_VERSION_170, true",
+		"JRE_VERSION_180, JRE_VERSION_180, true",
+		"JRE_VERSION_170, JRE_VERSION_160, true",
+		"JRE_VERSION_170, JRE_VERSION_170, true",
+		"JRE_VERSION_180, JRE_VERSION_9,   false",
+		"JRE_VERSION_170, JRE_VERSION_180, false",
+		"JRE_VERSION_170, JRE_VERSION_9,   false",
+	})
+	public void isAtLeast(@ConvertWith(JavaVersionConstantArgumentConverter.class) JavaVersion version1,
+			@ConvertWith(JavaVersionConstantArgumentConverter.class) JavaVersion version2, boolean result) {
+		assertEquals(version1.isAtLeast(version2), result);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"JRE_VERSION_160, JRE_VERSION_170, true",
+		"JRE_VERSION_170, JRE_VERSION_180, true",
+		"JRE_VERSION_180, JRE_VERSION_9,   true",
+		"JRE_VERSION_9,   JRE_VERSION_180, false",
+		"JRE_VERSION_170, JRE_VERSION_160, false",
+		"JRE_VERSION_170, JRE_VERSION_170, false",
+		"JRE_VERSION_180, JRE_VERSION_160, false",
+		"JRE_VERSION_180, JRE_VERSION_170, false",
+		"JRE_VERSION_180, JRE_VERSION_180, false",
+	})
+	public void isBelow(@ConvertWith(JavaVersionConstantArgumentConverter.class) JavaVersion version1,
+			@ConvertWith(JavaVersionConstantArgumentConverter.class) JavaVersion version2, boolean result) {
+		assertEquals(version1.isBelow(version2), result);
+	}
+
+
+	static class JavaVersionConstantArgumentConverter implements ArgumentConverter {
+		@Override
+		public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
+			try {
+				return JavaVersion.class.getField(String.valueOf(source)).get(null);
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				throw new ArgumentConversionException(e.toString());
+			}
+		}
+	}
 }
