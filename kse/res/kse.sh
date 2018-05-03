9ff831722f11967f3d216d8e9ce14f8bb3c32e93
Kai Kramer <kaikramer@users.noreply.github.com>
2018-05-03 16:14:37 +0200

    Fix illegal option error in kse.sh on macOS (issue #127)

--- kse/res/kse.sh
+++ kse/res/kse.sh
@@ -3,8 +3,14 @@
 # Oracle/OpenJDK JRE version 1.7+ has to be present on your path
 # OR in a directory called "jre" in the same directory as this script
 
-# detect parent folder of script, regardless aliases and symlinks
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+# detect real location of this script, regardless aliases and symlinks
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    REAL_SCRIPT_PATH=$(python -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' "$0")
+else
+    REAL_SCRIPT_PATH=$(readlink -f "$0")
+fi
+
+SCRIPT_DIR=$(dirname "$REAL_SCRIPT_PATH")
 JAR_FILE="${SCRIPT_DIR}/kse.jar"
 
 if [ -d "${SCRIPT_DIR}/jre" ]; then
