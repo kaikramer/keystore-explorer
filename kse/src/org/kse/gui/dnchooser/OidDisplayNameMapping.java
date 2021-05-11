cdab14dfdea7e7c76b53a7621380528aa7a1a442
Kai Kramer <kaikramer@users.noreply.github.com>
2021-05-11 15:53:53 +0200

    Added initials, pseudonym, DN qualifier and generation to DN chooser

--- kse/src/org/kse/gui/dnchooser/OidDisplayNameMapping.java
+++ kse/src/org/kse/gui/dnchooser/OidDisplayNameMapping.java
@@ -49,6 +49,10 @@ public class OidDisplayNameMapping {
 	private static final String NAME = res.getString("DistinguishedNameChooser.jlName.text");
 	private static final String STREET = res.getString("DistinguishedNameChooser.jlStreet.text");
 	private static final String TITLE = res.getString("DistinguishedNameChooser.jlTitle.text");
+	private static final String INITIALS = res.getString("DistinguishedNameChooser.jlInitials.text");
+	private static final String PSEUDONYM = res.getString("DistinguishedNameChooser.jlPseudonym.text");
+	private static final String DN_QUALIFIER = res.getString("DistinguishedNameChooser.jlDnQualifier.text");
+	private static final String GENERATION = res.getString("DistinguishedNameChooser.jlGeneration.text");
 
 	private static Map<String, ASN1ObjectIdentifier> displayNameToOID = new HashMap<>();
 	static {
@@ -68,7 +72,11 @@ public class OidDisplayNameMapping {
 
 		displayNameToOID.put(NAME, BCStyle.NAME);
 		displayNameToOID.put(STREET, BCStyle.STREET);
-        displayNameToOID.put(TITLE, BCStyle.T);
+		displayNameToOID.put(TITLE, BCStyle.T);
+		displayNameToOID.put(INITIALS, BCStyle.INITIALS);
+		displayNameToOID.put(PSEUDONYM, BCStyle.PSEUDONYM);
+		displayNameToOID.put(DN_QUALIFIER, BCStyle.DN_QUALIFIER);
+		displayNameToOID.put(GENERATION, BCStyle.GENERATION);
 	}
 
 	private static Map<String, String> oidToDisplayName = new HashMap<>();
@@ -90,10 +98,15 @@ public class OidDisplayNameMapping {
 		oidToDisplayName.put(BCStyle.NAME.getId(), NAME);
 		oidToDisplayName.put(BCStyle.STREET.getId(), STREET);
 		oidToDisplayName.put(BCStyle.T.getId(), TITLE);
+		oidToDisplayName.put(BCStyle.INITIALS.getId(), INITIALS);
+		oidToDisplayName.put(BCStyle.PSEUDONYM.getId(), PSEUDONYM);
+		oidToDisplayName.put(BCStyle.DN_QUALIFIER.getId(), DN_QUALIFIER);
+		oidToDisplayName.put(BCStyle.GENERATION.getId(), GENERATION);
 	}
 
 	public static String[] getDisplayNames() {
-		return new String[]{ CN, OU, O, L, ST, C, E, SN, GIVENNAME, SURNAME, DC, UID, NAME, STREET, TITLE };
+		return new String[] { CN, OU, O, L, ST, C, E, SN, GIVENNAME, SURNAME, DC, UID, NAME, STREET, TITLE, INITIALS,
+				PSEUDONYM, DN_QUALIFIER, GENERATION };
 	}
 
 	public static ASN1ObjectIdentifier getOidForDisplayName(String displayName) {
