7dc69e8b0c14f6d468335d68bbd51846fcb80afc
01101000 <Colbix@users.noreply.github.com>
2021-06-19 18:17:58 -0400

    Merge pull request #275 DH parameters feature
    * Update resources to reflect new GenerateDHParametersAction class
    
    * New action class for generating DH Parameters
    
    * New class for selecting DH Parameter settings.
    
    * New class for generating DH Parameters.
    
    * New class for viewing and exporting DH Parameters.
    
    * Updated resources to reflect new classes
    
    * Updated KseFrame to add the new Generate DHParameters feature.
    
    * Updated resources for the new DH Parameters feature
    
    * Updated to reflect new DH Parameters feature.
    
    * Revert "Updated KseFrame to add the new Generate DHParameters feature."
    
    * Restored missing resources accidently removed.
    
    * Updated KseFrame with new DH Parameters feature
    
    * Updated resources to reflect new DH Parameters
    
    * Restored accidently deleted lines.
    
    * Updated classes to improve documentation with Javadoc
    
    * Modified DH Parameters on the menus
    
    * Added dialogue text
    
    * Removed unneeded variable and formatted spaces to tabs.
    
    * Formatted spaces to tabs.
    
    * Added Javadoc and formatted spaces to tabs.
    
    * Modified GenerateDHParametersAction.statusbar text

---
+++ kse/src/org/kse/gui/dialogs/DGenerateDHParameters.java
@@ -0,0 +1,207 @@
+package org.kse.gui.dialogs;
+
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ResourceBundle;
+
+import javax.swing.AbstractAction;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+import org.kse.gui.JEscDialog;
+import org.kse.gui.PlatformUtil;
+import org.kse.utilities.DialogViewer;
+
+import net.miginfocom.swing.MigLayout;
+
+/**
+ * <h1> DH Parameters selection</h1>
+ * The DGenerateDHParameters class provides the user
+ * with the key size selection of DH Parameters to be
+ * generated.
+ * <p>
+ * The parameter generation is through the provider Bouncy Castle which
+ * uses a Sophie Germain search to identify a safe prime. The search
+ * method takes an exceptionally lengthy time with a selection above a 2048 bit
+ * key size. There is no technical limitation and the key size can be expanded
+ * if there is a demand for additional settings.
+ * <p>
+ * An expansion of this class can allow specific key sizes and the inclusion
+ * of DH standard RFC groups.
+ */
+
+public class DGenerateDHParameters extends JEscDialog{
+
+	private static final long serialVersionUID = 5909033737483232104L;
+
+	private static ResourceBundle res = ResourceBundle.getBundle("org/kse/gui/dialogs/resources");
+
+	private static final String CANCEL_KEY = "CANCEL_KEY";
+	private String[] keySizeSelection = {"1024", "2048"};
+	private JLabel jlDHKeySize;
+	private JComboBox<String> jcbDHKeySize;
+	
+	private JButton jbOK;
+	private JButton jbCancel;
+	private int dhKeySize;
+	private boolean success = false;
+
+	/**
+	 * Creates a new DGeneratingKeyPair dialog.
+	 *
+	 * @param parent The parent frame
+	 */
+	public DGenerateDHParameters(JFrame parent) {
+		super(parent, res.getString("DGenerateDHParameters.Title"), Dialog.ModalityType.DOCUMENT_MODAL);
+		initComponents();
+	}
+	
+	/**
+	 * Initializes the dialogue panel and associated elements 
+	 */
+	private void initComponents() {
+		
+		//TODO Generate DH Parameters icon
+		//ImageIcon icon = new ImageIcon(getClass().getResource("images/gendhp.png"));
+		//jlGenDHParam.setIcon(icon);
+		//jlGenDHParam.setHorizontalTextPosition(SwingConstants.LEADING);
+		//jlGenDHParam.setIconTextGap(15);
+		
+		jlDHKeySize = new JLabel(res.getString("DGenerateDHParameters.jlDHKeySize.text"));
+
+		jcbDHKeySize = new JComboBox<>();
+		jcbDHKeySize.setModel(new DefaultComboBoxModel<>(keySizeSelection));
+		jcbDHKeySize.setSelectedIndex(1);
+		jcbDHKeySize.setToolTipText(res.getString("DGenerateDHParameters.jcbDHKeySize.tooltip"));
+
+		jbCancel = new JButton(res.getString("DGenerateDHParameters.jbCancel.text"));
+		jbCancel.addActionListener(evt -> cancelPressed());
+		jbCancel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+				CANCEL_KEY);
+		jbCancel.getActionMap().put(CANCEL_KEY, new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				cancelPressed();
+			}
+		});
+
+		jbOK = new JButton(res.getString("DGenerateDHParameters.jbOK.text"));
+
+		JTextArea jtAreaFooter = new JTextArea();
+		jtAreaFooter = new JTextArea();
+		jtAreaFooter.setText(res.getString("DGenerateDHParameters.jtAreaFooter.text"));
+		jtAreaFooter.setToolTipText(res.getString("DGenerateDHParameters.jtAreaFooter.tooltip"));
+		jtAreaFooter.setEditable(false);
+		jtAreaFooter.setLineWrap(true);
+		jtAreaFooter.setWrapStyleWord(true);
+		jtAreaFooter.setBackground(getBackground());
+		
+		JPanel jpContent = new JPanel();
+		jpContent.setBorder(new TitledBorder(new EtchedBorder(), res.getString("DGenerateDHParameters.jpContent.text")));
+		JPanel buttons = PlatformUtil.createDialogButtonPanel(jbOK, jbCancel);
+
+		// layout
+		getContentPane().setLayout(new MigLayout("fill", "", ""));
+		getContentPane().add(jpContent, "wrap unrel, growx");
+		getContentPane().add(jtAreaFooter, "wrap unrel, growx");
+		getContentPane().add(buttons, "tag Ok, growx");
+
+		jpContent.setLayout(new MigLayout("insets dialog, ", "", "[]unrel[]"));
+		jpContent.add(jlDHKeySize, "");
+		jpContent.add(jcbDHKeySize, "growx, wrap");
+
+		jbOK.addActionListener(evt -> okPressed());
+		jbCancel.addActionListener(evt -> cancelPressed());
+		jbCancel.getActionMap().put(CANCEL_KEY, new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+
+			@Override public void actionPerformed(ActionEvent evt) {
+				cancelPressed();
+			}
+		});
+
+		addWindowListener(new WindowAdapter() {
+			@Override public void windowClosing(WindowEvent evt) {
+				closeDialog();
+			}
+		});
+		
+		setResizable(false);
+
+		getRootPane().setDefaultButton(jbOK);
+
+		pack();
+	}
+
+	/**
+	 * Set the selected key size.
+	 */
+	private void setKeySize() {
+			dhKeySize = Integer.parseInt((String) jcbDHKeySize.getSelectedItem());
+	}
+	
+	/**
+	 * Get the selected key size.
+	 *
+	 * @return The the key size value
+	 */
+	public int getKeySize() {	
+		return dhKeySize;
+	}
+
+	/**
+	 * Have the parameters been entered correctly?
+	 *
+	 * @return True if they have, false otherwise
+	 */
+	public boolean isSuccessful() {
+		return success;
+	}
+    /**
+     * Calls the set key size
+     * <p>
+     * Sets the success value to true
+     * <p>
+     * Calls the close dialogue 
+     */
+	private void okPressed() {
+		setKeySize();
+		success = true;
+		closeDialog();
+	}
+    /**
+     * Calls the close the dialogue
+     */
+	private void cancelPressed() {
+		closeDialog();
+	}
+	/**
+	 * Closes the dialogue 
+     */
+	private void closeDialog() {
+		setVisible(false);
+		dispose();
+	}
+
+	// for quick UI testing
+	public static void main(String[] args) throws Exception {
+		DGenerateDHParameters dialog = new DGenerateDHParameters(new JFrame());
+		DialogViewer.run(dialog);
+	}
+
+}
