1fa13ccde6b3cf718dc06409298daa3082da299a
Jairo Grateron <jgrateron@jgmail.com>
2021-07-29 19:06:53 -0400

    add find keystore entry

--- kse/src/org/kse/gui/KseFrame.java
+++ kse/src/org/kse/gui/KseFrame.java
@@ -62,6 +62,7 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
@@ -127,6 +128,7 @@ import org.kse.gui.actions.ExportKeyPairPrivateKeyAction;
 import org.kse.gui.actions.ExportKeyPairPublicKeyAction;
 import org.kse.gui.actions.ExportTrustedCertificateAction;
 import org.kse.gui.actions.ExportTrustedCertificatePublicKeyAction;
+import org.kse.gui.actions.FindAction;
 import org.kse.gui.actions.GenerateCsrAction;
 import org.kse.gui.actions.GenerateDHParametersAction;
 import org.kse.gui.actions.GenerateKeyPairAction;
@@ -249,6 +251,7 @@ public final class KseFrame implements StatusBar {
 	private JMenuItem jmiCut;
 	private JMenuItem jmiCopy;
 	private JMenuItem jmiPaste;
+	private JMenuItem jmiFind;
 
 	private JMenu jmView;
 	private JCheckBoxMenuItem jcbmiShowHideToolBar;
@@ -442,6 +445,7 @@ public final class KseFrame implements StatusBar {
 	private final CutAction cutAction = new CutAction(this);
 	private final CopyAction copyAction = new CopyAction(this);
 	private final PasteAction pasteAction = new PasteAction(this);
+	private final FindAction findAction = new FindAction(this);
 	private final ShowHideToolBarAction showHideToolBarAction = new ShowHideToolBarAction(this);
 	private final ShowHideStatusBarAction showHideStatusBarAction = new ShowHideStatusBarAction(this);
 	private final TabStyleWrapAction tabStyleWrapAction = new TabStyleWrapAction(this);
@@ -781,6 +785,12 @@ public final class KseFrame implements StatusBar {
 		new StatusBarChangeHandler(jmiPaste, (String) pasteAction.getValue(Action.LONG_DESCRIPTION), this);
 		jmEdit.add(jmiPaste);
 
+		jmiFind = new JMenuItem(findAction);
+		PlatformUtil.setMnemonic(jmiFind, res.getString("KseFrame.jmiFind.mnemonic").charAt(0));
+		jmiFind.setToolTipText(null);
+		new StatusBarChangeHandler(jmiFind, (String) findAction.getValue(Action.LONG_DESCRIPTION), this);
+		jmEdit.add(jmiFind);
+		
 		jmView = new JMenu(res.getString("KseFrame.jmView.text"));
 		PlatformUtil.setMnemonic(jmView, res.getString("KseFrame.jmView.mnemonic").charAt(0));
 
@@ -2763,6 +2773,9 @@ public final class KseFrame implements StatusBar {
 			updateCutCopyPasteControls();
 		}
 
+		// Can find 
+		findAction.setEnabled(true);
+
 		// Can use tools on
 		generateKeyPairAction.setEnabled(true);
 		generateSecretKeyAction.setEnabled(type.supportsKeyEntries());
@@ -2886,6 +2899,7 @@ public final class KseFrame implements StatusBar {
 		cutKeyPairAction.setEnabled(false);
 		cutTrustedCertificateAction.setEnabled(false);
 		copyAction.setEnabled(false);
+		findAction.setEnabled(false);
 		copyKeyPairAction.setEnabled(false);
 		copyTrustedCertificateAction.setEnabled(false);
 		pasteAction.setEnabled(false);
---
+++ kse/src/org/kse/gui/actions/FindAction.java
@@ -0,0 +1,89 @@
+package org.kse.gui.actions;
+
+import java.awt.Toolkit;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.util.Enumeration;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
+
+import org.kse.gui.KseFrame;
+import org.kse.gui.dialogs.DFindKeyStoreEntry;
+import org.kse.gui.error.DError;
+import org.kse.utilities.history.KeyStoreHistory;
+
+/**
+ * Action to Find a KeyStore entry.
+ *
+ */
+public class FindAction extends KeyStoreExplorerAction {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construct action
+	 * 
+	 * @param kseFrame KeyStore Explorer frame
+	 */
+	public FindAction(KseFrame kseFrame) {
+		super(kseFrame);
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(res.getString("FindAction.accelerator").charAt(0),
+				Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+		putValue(LONG_DESCRIPTION, res.getString("FindAction.statusbar"));
+		putValue(NAME, res.getString("FindAction.text"));
+		putValue(SHORT_DESCRIPTION, res.getString("FindAction.tooltip"));
+		putValue(SMALL_ICON,
+				new ImageIcon(Toolkit.getDefaultToolkit().createImage(getClass().getResource("images/copy.png"))));
+	}
+
+	@Override
+	protected void doAction() {
+
+		DFindKeyStoreEntry dialog = new DFindKeyStoreEntry(frame);
+		dialog.setLocationRelativeTo(frame);
+		dialog.setVisible(true);
+
+		if (dialog.isSuccess()) {
+			try {
+				String previousAlias = kseFrame.getSelectedEntryAlias();
+				String alias = findEntryAlias(previousAlias, dialog.getEntryName());
+				if (alias == null && previousAlias != null) {
+					alias = findEntryAlias(null, dialog.getEntryName());
+				}
+				if (alias == null) {
+					JOptionPane.showMessageDialog(frame, dialog.getEntryName() + " " + res.getString("FindAction.NotFound.message"),
+							res.getString("FindAction.Find.Title"),
+							JOptionPane.WARNING_MESSAGE);					
+				}
+				else {
+					kseFrame.setSelectedEntriesByAliases(alias);
+				}
+			} catch (KeyStoreException ex) {
+				DError.displayError(frame, ex);
+			}
+		}
+	}
+
+	private String findEntryAlias(String previousAlias, String name) throws KeyStoreException {
+		KeyStoreHistory history = kseFrame.getActiveKeyStoreHistory();
+		KeyStore keyStore = history.getCurrentState().getKeyStore();
+
+		boolean search = true;
+		if (previousAlias != null) {
+			search = false;
+		}
+		Enumeration<String> enumeration = keyStore.aliases();
+		while (enumeration.hasMoreElements()) {
+			String alias = enumeration.nextElement();
+			if (alias.equals(previousAlias)) {
+				search = true;
+			} else {
+				if (search && alias.contains(name)) {
+					return alias;
+				}
+			}
+		}
+		return null;
+	}
+}
