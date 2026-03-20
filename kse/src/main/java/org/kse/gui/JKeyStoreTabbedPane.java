0083a31b10f3324083970e687f44d188d1124bca
Jairo Grateron <jgrateron@gmail.com>
2026-03-20 10:28:30 -0400

    Add global keyboard shortcuts for tab navigation and closing
    Implemented a global KeyEventDispatcher in JKeyStoreTabbedPane to handle tab navigation shortcuts (Ctrl+PageUp / Ctrl+PageDown) and tab closing (Ctrl+F4).

--- kse/src/main/java/org/kse/gui/JKeyStoreTabbedPane.java
+++ kse/src/main/java/org/kse/gui/JKeyStoreTabbedPane.java
@@ -20,7 +20,6 @@
 
 package org.kse.gui;
 
-import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
@@ -46,7 +45,45 @@ public class JKeyStoreTabbedPane extends JTabbedPane implements DropTargetListen
         this.kseFrame = kseFrame;
 
         // Make this pane a drop target and its own listener
-        new DropTarget(this, this);
+        new java.awt.dnd.DropTarget(this, this);
+
+        java.awt.KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new java.awt.KeyEventDispatcher() {
+            @Override
+            public boolean dispatchKeyEvent(java.awt.event.KeyEvent e) {
+                if (JKeyStoreTabbedPane.this.kseFrame.getUnderlyingFrame() != null &&
+                    JKeyStoreTabbedPane.this.kseFrame.getUnderlyingFrame().isActive()) {
+
+                    if (e.getID() == java.awt.event.KeyEvent.KEY_PRESSED && e.isControlDown()) {
+                        
+                        if (e.getKeyCode() == java.awt.event.KeyEvent.VK_PAGE_UP) {
+                            int index = getSelectedIndex();
+                            if (index > 0) {
+                                setSelectedIndex(index - 1);
+                            } else if (getTabCount() > 0) {
+                                setSelectedIndex(getTabCount() - 1);
+                            }
+                            return true;
+                        }
+                        
+                        if (e.getKeyCode() == java.awt.event.KeyEvent.VK_PAGE_DOWN) {
+                            int index = getSelectedIndex();
+                            if (index < getTabCount() - 1) {
+                                setSelectedIndex(index + 1);
+                            } else if (getTabCount() > 0) {
+                                setSelectedIndex(0);
+                            }
+                            return true;
+                        }
+
+                        if (e.getKeyCode() == java.awt.event.KeyEvent.VK_F4) {
+                            new org.kse.gui.actions.CloseAction(JKeyStoreTabbedPane.this.kseFrame).closeActiveKeyStore();
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        });
     }
 
     @Override
